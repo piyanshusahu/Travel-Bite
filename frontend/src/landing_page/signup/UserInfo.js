16877c99bee8629b9a256db2944a7633f8280e34
piyanshu sahu <sahupiyanshu.28@gmail.com>
2025-06-15 15:41:54 +0530

    updated

--- frontend/src/landing_page/signup/UserInfo.js
+++ frontend/src/landing_page/signup/UserInfo.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import * as Yup from "yup";
-import { useState, useEffect } from "react";
-import { Formik, useFormik } from "formik";
+import { useFormik } from "formik";
 import "./UserInfo.css";
-import { FaUser } from "react-icons/fa";
-import { FaLock } from "react-icons/fa";
+import { FaUser, FaLock } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { BiSolidUserCircle } from "react-icons/bi";
-import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
-import Login from "../Login";
+import "react-toastify/dist/ReactToastify.css";
 
 const SignupSchema = Yup.object().shape({
-  firstName: Yup.string().required("Required"),
-  lastName: Yup.string().required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"),
+  firstName: Yup.string().required("First name is required"),
+  lastName: Yup.string().required("Last name is required"),
+  gender: Yup.string()
+    .oneOf(["male", "female"], "Select a valid gender")
+    .required("Gender is required"),
   userName: Yup.string()
-    .required("Required")
-    .min(5, "Short")
-    .max(50, "Too Long"),
-  password: Yup.string().required("Required!").min(8, "Weak Password"),
+    .required("Username is required")
+    .min(5, "Username is too short")
+    .max(50, "Username is too long"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  phone: Yup.string()
+    .matches(/^[0-9]{10}$/, "Phone number must be 10 digits")
+    .required("Phone number is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]+$/,
+      "Password must contain letters and numbers"
+    ),
 });
 
 function UserInfo() {
@@ -29,99 +38,74 @@ function UserInfo() {
     initialValues: {
       firstName: "",
       lastName: "",
-      Gender: "null",
+      gender: "",
       userName: "",
       email: "",
       phone: "",
       password: "",
     },
     validationSchema: SignupSchema,
+    onSubmit: (values) => {
+      axios
+        .post("http://localhost:3002/signup", values)
+        .then(() => {
+          toast.success("Signup Successful!");
+        })
+        .catch(() => {
+          toast.error("Signup Failed!");
+        });
+    },
+    validateOnMount: true,
   });
-  const submitInfo = () => {
-    if (formik.values.firstName === "") {
-      alert("Required first name");
-    } 
-      axios.post("http://localhost:3002/signup", {
-        firstName: formik.values.firstName,
-        lastName: formik.values.lastName,
-        gender: formik.values.gender,
-        userName: formik.values.userName,
-        email: formik.values.email,
-        phone: formik.values.phone,
-        password: formik.values.password,
-        isRemember: formik.values.isRemember,
-      });
-      };
-
-    const [isSubmitActive,setIsSubmitActive]=useState(false);
 
   return (
     <div className="signup">
-      <div
-        className="outerDiv"
-        style={{
-          fontFamily: "Poppins,sans-serif",
-          webkitFontSmoothing: "antialiased",
-          mozOsxFontSmoothing: "grayscale",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          padding: "10px 10px 10px 10px",
-        }}
-      >
+      <ToastContainer />
+      <div className="outerDiv">
         <div className="wrapper mb-5">
-          <form onSubmit={formik.handleSubmit}>
+          <form onSubmit={formik.handleSubmit} noValidate>
             <h1>Create a new account</h1>
+
             <div className="input-box">
               <input
                 type="text"
                 placeholder="First Name"
-                onChange={formik.handleChange}
-                value={formik.values.firstName}
                 name="firstName"
-                required
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.firstName}
               />
-
               <FaUser className="icon" />
             </div>
+            {formik.touched.firstName && formik.errors.firstName && (
+              <div className="error">{formik.errors.firstName}</div>
+            )}
 
             <div className="input-box">
               <input
                 type="text"
                 placeholder="Last Name"
-                onChange={formik.handleChange}
-                value={formik.values.lastName}
                 name="lastName"
-                required
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.lastName}
               />
               <FaUser className="icon" />
             </div>
+            {formik.touched.lastName && formik.errors.lastName && (
+              <div className="error">{formik.errors.lastName}</div>
+            )}
 
-            <div
-              className="gender"
-              style={{
-                marginTop: "-15px",
-                marginBottom: "-15px",
-              }}
-            >
-              <h6
-                style={{
-                  display: "inline",
-                  marginRight: "20px",
-                  marginLeft: "50px",
-                }}
-              >
-                Gender
-              </h6>
-
+            <div className="gender">
+              <h6 style={{ display: "inline", marginRight: "20px" }}>Gender</h6>
               <label style={{ marginRight: "30px" }}>
                 <input
                   type="radio"
                   name="gender"
                   value="male"
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  checked={formik.values.gender === "male"}
                 />{" "}
                 Male
               </label>
@@ -131,83 +115,104 @@ function UserInfo() {
                   name="gender"
                   value="female"
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  checked={formik.values.gender === "female"}
                 />{" "}
                 Female
               </label>
             </div>
+            {formik.touched.gender && formik.errors.gender && (
+              <div className="error">{formik.errors.gender}</div>
+            )}
 
             <div className="input-box">
               <input
                 type="text"
                 placeholder="Username"
-                onChange={formik.handleChange}
-                value={formik.values.userName}
                 name="userName"
-                required
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.userName}
               />
               <BiSolidUserCircle className="icon" />
             </div>
+            {formik.touched.userName && formik.errors.userName && (
+              <div className="error">{formik.errors.userName}</div>
+            )}
 
             <div className="input-box">
               <input
                 type="text"
                 placeholder="E-mail Address"
-                onChange={formik.handleChange}
-                value={formik.values.email}
                 name="email"
-                required
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
               />
               <MdEmail className="icon" />
             </div>
+            {formik.touched.email && formik.errors.email && (
+              <div className="error">{formik.errors.email}</div>
+            )}
 
             <div className="input-box">
               <input
                 type="text"
                 placeholder="Phone Number"
-                onChange={formik.handleChange}
-                value={formik.values.phone}
                 name="phone"
-                required
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.phone}
               />
               <BsFillTelephoneFill className="icon" />
             </div>
+            {formik.touched.phone && formik.errors.phone && (
+              <div className="error">{formik.errors.phone}</div>
+            )}
 
             <div className="input-box">
               <input
                 type="password"
                 placeholder="Password"
-                onChange={formik.handleChange}
-                value={formik.values.password}
                 name="password"
-                required
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
               />
               <FaLock className="icon" />
             </div>
+            {formik.touched.password && formik.errors.password && (
+              <div className="error">{formik.errors.password}</div>
+            )}
 
-            <div className="remember-forgot" style={{marginLeft:"60%"}}>
+            <div className="remember-forgot" style={{ marginLeft: "60%" }}>
               <a href="#">Forget Password?</a>
             </div>
+
             <div
-            //onClick={handleNext}
-            className={`searchOptions flex`}
-            style={{
-              opacity: isSubmitActive ? 1 : 0.9,
-              cursor: isSubmitActive ? "pointer" : "not-allowed",
-              // pointerEvents: isSearchActive ? "auto" : "none",
-            }}
-          >
-            <Link
-              to={isSubmitActive ? "./budget" : "#"}
-              style={{ pointerEvents: isSubmitActive ? "auto" : "none" }}
+              className="searchOptions flex"
+              style={{
+                opacity: formik.isValid ? 1 : 0.6,
+                cursor: formik.isValid ? "pointer" : "not-allowed",
+              }}
             >
-              <span style={{ color: "white" }}>Submit</span>
-            </Link>
-          </div>
+              <button
+                type="submit"
+                disabled={!formik.isValid}
+                style={{ background: "none", border: "none" }}
+              >
+                <span style={{ color: "white" }}>Submit</span>
+              </button>
+            </div>
+
             <div className="register-link">
               <p>
                 Already have an account? <a href="">Log in</a>
               </p>
             </div>
+
+            {/* Optional Debug Info */}
+            {/* <pre>{JSON.stringify(formik, null, 2)}</pre> */}
           </form>
         </div>
       </div>
