d3d0f8d0064e22130b5946bf97b1035ebd13ce00
aksharkanodia123 <kanodiaakshar@gmail.com>
2025-07-02 14:02:29 +0530

    Changes

--- frontend/src/itenary/budget/HotelCard.jsx
+++ frontend/src/itenary/budget/HotelCard.jsx
@@ -10,6 +10,7 @@ import {
   FaHotTub,
   FaDog,
   FaWheelchair,
+  FaStar
 } from "react-icons/fa";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareArrowUpRight } from "@fortawesome/free-solid-svg-icons";
